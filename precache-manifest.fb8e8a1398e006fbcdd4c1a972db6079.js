81f54372fdf8ea992adbb3af00d9938744839e74
BHARATH SIMHA REDDY <bharathreddy8345@gmail.com>
2020-09-05 10:03:50 +0530

    Updates

---
+++ precache-manifest.fb8e8a1398e006fbcdd4c1a972db6079.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a839b87a6ca4390623c5a070e18c87d3",
+    "url": "/MonsterSearch/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/MonsterSearch/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b19073708b59a8d51378",
+    "url": "/MonsterSearch/static/css/main.3b197efb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/MonsterSearch/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterSearch/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19073708b59a8d51378",
+    "url": "/MonsterSearch/static/js/main.b1b8b9c1.chunk.js"
+  },
+  {
+    "revision": "76a1f666e5d65a1863dd",
+    "url": "/MonsterSearch/static/js/runtime-main.e4f9e469.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/MonsterSearch/static/media/SEGA.12a3db8b.woff"
+  }
+]);
